b10a925f37fdabb2b8ceff636e854434e0687151
stefanmallaun <stmallaun@tsn.at>
2023-09-27 15:19:50 +0200

    Aufgabe1NochNichtFertig

---
+++ 01_Numpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])
+
+#1.1.1
+b = np.arange(100, 201)
+
+#1.1.2
+b = np.arange(100, 201, 2)
+
+#1.1.3
+c = np.arange(100, 111, 0.5)
+
+#1.1.4
+#Normalverteilung
+d = np.random.normal(0, 100, 100)
+
+print(d)
