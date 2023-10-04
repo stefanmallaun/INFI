af0246cb7cda2dd99ce2c4fa96ead2b7c8c86b0c
stefanmallaun <stmallaun@tsn.at>
2023-10-04 14:12:08 +0200

    Aufgabe1 fertig

--- 01_Numpy.py
+++ 01_Numpy.py
@@ -15,4 +15,26 @@ c = np.arange(100, 111, 0.5)
 #Normalverteilung
 d = np.random.normal(0, 100, 100)
 
-print(d)
+
+#Einfache Berechnung 
+#Mittelwert
+e = np.mean(d)
+#Median
+f = np.median(d)
+#Minimum
+g = np.min(d)
+#Maximum
+h = np.max(d)
+#Standard deviation
+i = np.std(d)
+#In welchem Bereich liegen die mittleren 95%? Welche Werte sind das die außerhalb liegen?
+j = np.percentile(d, 95)
+#Alle Zahlen mit 100 mulitplieren
+k = d*100
+#Nur die ersten 10 Zahlen auswählen
+l = d[:10]
+#Nur die Zahlen auswählen, die größer 0 sind
+m = d[d>0]
+
+
+print(a, b, c, d, e, f, g, h, i, j, k)
